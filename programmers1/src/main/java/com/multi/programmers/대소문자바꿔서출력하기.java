b91c9d3cf4a9a57152366fb7dd2aebb4797fb0ca
DESKTOP-VFR14VJ\juan <pjw03659@naver.com>
2023-06-27 11:41:28 +0900

    Feat : Programmers 문제풀이

---
+++ programmers1/src/main/java/com/multi/programmers/대소문자바꿔서출력하기.java
@@ -0,0 +1,21 @@
+package com.multi.programmers;
+
+import java.util.Scanner;
+
+public class 대소문자바꿔서출력하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String result="";
+		
+		for(int i=0; i<a.length();i++) {
+			char c = a.charAt(i);
+			if(Character.isLowerCase(c)) {
+				result+= Character.toUpperCase(c);
+			} else {
+				result+= Character.toLowerCase(c);
+			}
+		}
+		System.out.println(result);
+	}
+}
