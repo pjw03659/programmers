de34a9af5a661ec287a984edd110f9308ae2f933
DESKTOP-VFR14VJ\juan <pjw03659@naver.com>
2023-06-28 21:31:13 +0900

    Feat : 프로그래머스 문제풀이

---
+++ programmers1/src/main/java/com/multi/programmers/덧셈식출력하기.java
@@ -0,0 +1,12 @@
+package com.multi.programmers;
+
+import java.util.Scanner;
+public class µ¡¼À½ÄÃâ·ÂÇÏ±â {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+
+	        System.out.println(a + " + " + b + " = " + (a+b));
+	    }
+}
---
+++ programmers1/src/main/java/com/multi/programmers/홀짝구분하기.java
@@ -0,0 +1,13 @@
+package com.multi.programmers;
+import java.util.Scanner;
+public class È¦Â¦±¸ºÐÇÏ±â {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0) {
+        	System.out.println(n+" is even");
+        }else {
+        	System.out.println(n+" is odd");
+        }
+    }
+}
